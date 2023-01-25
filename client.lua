1eea42d700082cb42dcc2e14ab78325c65754b91
meuntouchable <dortey@outlook.de>
2023-01-25 21:11:32 +0100

    upload

---
+++ client.lua
@@ -0,0 +1,27 @@
+function ScarNotify(msg,typ,audio,time)
+	SendNUIMessage({
+		action="ScarNotify->Message",
+        message=msg,
+        options={
+            type=typ or "info",
+            timeout=time or 5000,
+            audio=audio or true,
+        }
+	});
+end
+
+RegisterNetEvent("ScarNotify->Trigger")
+AddEventHandler("ScarNotify->Trigger",function(msg,typ,audio,time)
+	return ScarNotify(msg,typ,audio,time);
+end)
+
+
+RegisterCommand("testo",function(source,args)
+	--ScarNotify("SCAR Scripts on #1","info",true,7000);
+	TriggerEvent("ScarNotify->Trigger","SCAR Scripts on #1","info",true,7000)
+end,false)
+
+
+
+
+--exports.freeroamR_notify:Info("Vehicle successfully spawned!");
